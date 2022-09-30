379618ccc1f5c875f5bfb9e58b16aa7c13e510f1
elena-gireva <94042128+elena-gireva@users.noreply.github.com>
2022-09-30 16:46:30 +0300

    add commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react';
+import woman from './woman.jpg';
 
 function App() {
+
+const [hobby, setHobby] = useState('');
+
+useEffect(() => {
+
+  getHobby()
+
+},[])
+
+
+
+const getHobby = async() => {
+  const responce = await fetch('http://www.boredapi.com/api/activity/');
+  const data = await responce.json();
+  setHobby(data)
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+        <h2 class="animate__animated animate__slideInUp">What I want to do?</h2>
+      
+      <img src={woman} alt="bored woman" width='350px'/>
+
+      <h1 class="animate__animated animate__backInLeft">{hobby.activity}</h1>
+      <button onClick={getHobby}>WOW and maybe something else? (click)</button>
     </div>
+  
   );
 }
 
